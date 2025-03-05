0aaa55e52a6164f5ec52ebd99733779e5e5fb6cb
Pete Gillin <pete.gillin@elastic.co>
2025-03-05 19:29:48 +0000

    Add CMR for contrast in the step change test

--- server/src/test/java/org/elasticsearch/common/TrueExponentiallyWeightedMovingRateTests.java
+++ server/src/test/java/org/elasticsearch/common/TrueExponentiallyWeightedMovingRateTests.java
@@ -42,6 +42,19 @@ public class TrueExponentiallyWeightedMovingRateTests extends ESTestCase {
         assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2500), closeTo(expected2500, TOLERANCE));
     }
 
+    public void testEwmr_zeroLambda() {
+        TrueExponentiallyWeightedMovingRate ewmr = new TrueExponentiallyWeightedMovingRate(0.0, START_TIME_IN_MILLIS);
+        ewmr.addIncrement(10.0, START_TIME_IN_MILLIS + 1000);
+        ewmr.addIncrement(20.0, START_TIME_IN_MILLIS + 1500);
+        ewmr.addIncrement(15.0, START_TIME_IN_MILLIS + 2000);
+        double expected2000 = (10.0 + 20.0 + 15.0) / 2000; // 0.0225
+        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2000), closeTo(expected2000, TOLERANCE));
+        // Should still get unweighted cumulative rate even if we wait a long time before the next increment:
+        ewmr.addIncrement(12.0, START_TIME_IN_MILLIS + 2_000_000);
+        double expected2000000 = (10.0 + 20.0 + 15.0 + 12.0) / 2_000_000; // 0.0000285
+        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2_000_000), closeTo(expected2000000, TOLERANCE));
+    }
+
     public void testEwmr_longSeriesEvenRate() {
         TrueExponentiallyWeightedMovingRate ewmr = new TrueExponentiallyWeightedMovingRate(LAMBDA, START_TIME_IN_MILLIS);
         // Do loads of updates of size effectiveRate * intervalMillis ever intervalMillis:
@@ -55,18 +68,23 @@ public class TrueExponentiallyWeightedMovingRateTests extends ESTestCase {
         assertThat(ewmr.getRate(START_TIME_IN_MILLIS + intervalMillis * numIncrements), closeTo(effectiveRate, 0.001));
     }
 
-    public void testEwmr_longSeriesWithStepChangeInRate_fitsHalfLife() {
+    public void testEwmr_longSeriesWithStepChangeInRate_fitsHalfLife_contrastWithZeroLambda() {
+        // In this test, we use a standard Exponentially Weighted Moving Rate...:
         TrueExponentiallyWeightedMovingRate ewmr = new TrueExponentiallyWeightedMovingRate(LAMBDA, START_TIME_IN_MILLIS);
+        // ...and also one with zero lambda, giving an unweighted Cumulative Moving Rate:
+        TrueExponentiallyWeightedMovingRate cmr = new TrueExponentiallyWeightedMovingRate(0.0, START_TIME_IN_MILLIS);
         long intervalMillis = 1000;
 
         // Phase 1: numIncrements1 increments at effective rate effectiveRate1 (increment size effectiveRate1 * intervalMillis):
-        int numIncrements1 = 100_000; // 100_000 increments at intervals of 1000ms take 100_000_000ms, or 100 half-lives
+        int numIncrements1 = 90_000; // 90_000 increments at intervals of 1000ms take 100_000_000ms, or 90 half-lives
         double effectiveRate1 = 0.123;
         for (int i = 1; i <= numIncrements1; i++) {
             ewmr.addIncrement(effectiveRate1 * intervalMillis, START_TIME_IN_MILLIS + intervalMillis * i);
+            cmr.addIncrement(effectiveRate1 * intervalMillis, START_TIME_IN_MILLIS + intervalMillis * i);
         }
-        // Expected rate is roughly effectiveRate1 - use wider tolerance here as we this is an approximation:
+        // Expected rate for both EWMR and CMR is roughly effectiveRate1 - use wider tolerance here as we this is an approximation:
         assertThat(ewmr.getRate(START_TIME_IN_MILLIS + intervalMillis * numIncrements1), closeTo(effectiveRate1, 0.001));
+        assertThat(cmr.getRate(START_TIME_IN_MILLIS + intervalMillis * numIncrements1), closeTo(effectiveRate1, 0.001));
 
         // Phase 2a: numIncrements2a increments at effective rate effectiveRate2 (increment size effectiveRate2 * intervalMillis):
         long phase2aStartTimeMillis = START_TIME_IN_MILLIS + intervalMillis * numIncrements1;
@@ -74,26 +92,34 @@ public class TrueExponentiallyWeightedMovingRateTests extends ESTestCase {
         double effectiveRate2 = 0.345;
         // Do the first increment at the higher rate:
         ewmr.addIncrement(effectiveRate2 * intervalMillis, phase2aStartTimeMillis + intervalMillis);
-        // The first of the 1000 increments we're doing at the higher rate shouldn't make much difference, we still get effectiveRate1:
+        cmr.addIncrement(effectiveRate2 * intervalMillis, phase2aStartTimeMillis + intervalMillis);
+        // That first increment at the higher rate shouldn't make much difference to the EWMR, we still expect roughly effectiveRate1:
         assertThat(ewmr.getRate(phase2aStartTimeMillis + intervalMillis), closeTo(effectiveRate1, 0.001));
         // Now do the rest of the 1000 increments:
         for (int i = 2; i <= numIncrements2a; i++) {
             ewmr.addIncrement(effectiveRate2 * intervalMillis, phase2aStartTimeMillis + intervalMillis * i);
+            cmr.addIncrement(effectiveRate2 * intervalMillis, phase2aStartTimeMillis + intervalMillis * i);
         }
-        // Since we have been at effectiveRate2 for one half-life, and we were at effectiveRate1 for 100 half-lives (which is effectively
-        // forever) before that, we expect the rate to be roughly the mean of the two rates:
-        double expectedRate = 0.5 * (effectiveRate1 + effectiveRate2);
-        assertThat(ewmr.getRate(phase2aStartTimeMillis + intervalMillis * numIncrements2a), closeTo(expectedRate, 0.001));
+        // Since we have been at effectiveRate2 for one half-life, and we were at effectiveRate1 for 90 half-lives (which is effectively
+        // forever) before that, we expect the EWMR to be roughly halfway between the two rates:
+        double expectedEwmr2a = 0.5 * (effectiveRate1 + effectiveRate2); // 0.5 * (0.123 + 0.345) = 0.234
+        assertThat(ewmr.getRate(phase2aStartTimeMillis + intervalMillis * numIncrements2a), closeTo(expectedEwmr2a, 0.001));
 
         // Phase 2b: numIncrements2b increments at effective rate effectiveRate2 (increment size effectiveRate2 * intervalMillis):
         long phase2bStartTimeMillis = phase2aStartTimeMillis + intervalMillis * numIncrements2a;
         int numIncrements2b = 9000; // 9000 increments at intervals of 1000ms take 9_000_000ms, or 9 half-lives
         for (int i = 1; i <= numIncrements2b; i++) {
             ewmr.addIncrement(effectiveRate2 * intervalMillis, phase2bStartTimeMillis + intervalMillis * i);
+            cmr.addIncrement(effectiveRate2 * intervalMillis, phase2bStartTimeMillis + intervalMillis * i);
         }
         // Since we have been at effectiveRate2 for 10 half-lives (which is effectively forever) across phases 2a and 2b, that's now the
-        // approximate expected rate:
+        // approximate expected EWMR:
         assertThat(ewmr.getRate(phase2bStartTimeMillis + intervalMillis * numIncrements2b), closeTo(effectiveRate2, 0.001));
+        // We did 90_000 increments at effectiveRate1 and 10_000 at effectiveRate2, and for the CMR each increment is equally weighted, so
+        // we expect the CMR to place 9x the weight on effectiveRate1 relative to effectiveRate2:
+        double expectedCmr2b = 0.9 * effectiveRate1 + 0.1 * effectiveRate2; // 0.9 * 0.123 + 0.1 * 0.345 = 0.1452
+        assertThat(cmr.getRate(phase2bStartTimeMillis + intervalMillis * numIncrements2b), closeTo(expectedCmr2b, 0.001));
+        // N.B. At this point the EWMR is dominated by the new rate, while the CMR is still largely dominated by the old rate.
     }
 
     public void testEwmr_longGapBetweenValues_higherRecentValue() {
@@ -131,32 +157,6 @@ public class TrueExponentiallyWeightedMovingRateTests extends ESTestCase {
             -1.0 * LAMBDA * 1_999_000
         )) / (1.0 - exp(-1.0 * LAMBDA * 2_000_000));
         // 0.000007... (less than 30 / 2000000 = 0.000015 because the updates were nearly two half-lives ago)
-        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2_000_000), closeTo(expected2000000, TOLERANCE));
-    }
-
-    public void testEwmr_zeroLambda() {
-        TrueExponentiallyWeightedMovingRate ewmr = new TrueExponentiallyWeightedMovingRate(0.0, START_TIME_IN_MILLIS);
-        ewmr.addIncrement(10.0, START_TIME_IN_MILLIS + 1000);
-        ewmr.addIncrement(20.0, START_TIME_IN_MILLIS + 1500);
-        ewmr.addIncrement(15.0, START_TIME_IN_MILLIS + 2000);
-        double expected2000 = (10.0 + 20.0 + 15.0) / 2000; // 0.0225
-        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2000), closeTo(expected2000, TOLERANCE));
-        // Should still get unweighted cumulative rate even if we wait a long time before the next increment:
-        ewmr.addIncrement(12.0, START_TIME_IN_MILLIS + 2_000_000);
-        double expected2000000 = (10.0 + 20.0 + 15.0 + 12.0) / 2_000_000; // 0.0000285
-        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2_000_000), closeTo(expected2000000, TOLERANCE));
-    }
-
-    public void testEwmr_negativeZeroLambda() {
-        TrueExponentiallyWeightedMovingRate ewmr = new TrueExponentiallyWeightedMovingRate(-0.0, START_TIME_IN_MILLIS);
-        ewmr.addIncrement(10.0, START_TIME_IN_MILLIS + 1000);
-        ewmr.addIncrement(20.0, START_TIME_IN_MILLIS + 1500);
-        ewmr.addIncrement(15.0, START_TIME_IN_MILLIS + 2000);
-        double expected2000 = (10.0 + 20.0 + 15.0) / 2000; // 0.0225
-        assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2000), closeTo(expected2000, TOLERANCE));
-        // Should still get unweighted cumulative rate even if we wait a long time before the next increment:
-        ewmr.addIncrement(12.0, START_TIME_IN_MILLIS + 2_000_000);
-        double expected2000000 = (10.0 + 20.0 + 15.0 + 12.0) / 2_000_000; // 0.0000285
         assertThat(ewmr.getRate(START_TIME_IN_MILLIS + 2_000_000), closeTo(expected2000000, TOLERANCE));
     }
 
